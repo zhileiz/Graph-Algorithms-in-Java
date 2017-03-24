ac101a9acc0aee23bad7b2a73d1804d8d1ef5710
zhileiz <zhileiz@seas.upenn.edu>
2017-03-23 22:27:25 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Breadth First Search
+        System.out.println("##### BFS #####");
+
+
+
+
+
+        // Depth First Search
+        System.out.println("##### DFS #####");
+
+
+
+
+
+        // Dijkstra Algorithm
+        System.out.println("##### Dijkstra #####");
+
+
+
+
+
+        // Prim Algorithm
+        System.out.println("##### Prim #####");
+
+
+
+
+
+        // K-Core Algorithm
+        System.out.println("##### K-Core #####");
+
+
+
+
+
+    }
+}
